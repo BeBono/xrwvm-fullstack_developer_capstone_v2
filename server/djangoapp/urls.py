cd8d286c92102346a92c1086943bc5cbdcac1574
BeBono <gitbebono@gmail.com>
2024-04-29 11:42:39 -0400

    About.html Ok

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,7 +7,6 @@ from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path('about/', TemplateView.as_view(template_name="About.html")),
     # # path for registration
 
     # path for login
