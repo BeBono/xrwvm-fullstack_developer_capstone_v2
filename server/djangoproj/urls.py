c2108c4c15fac8edc546b251c8405e3c519013d1
BeBono <gitbebono@gmail.com>
2024-05-24 14:27:08 -0400

    Backend integrated Sentiment Analizer and MongoDB

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -29,6 +29,13 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     
     
-    path(route='get_cars', view=views.get_cars, name ='getcars'), #*******************Added by mine
+    path(route='get_cars', view=views.get_cars, name ='getcars'), #*******************Added by mine to populate the DealerCar and DealerMake
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+
+
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
